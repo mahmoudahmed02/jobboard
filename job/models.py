b964c075f934680018fdc695500602e946f8fbb3
mahmoudahmed02 <mahmoud.ah@hotmail.com>
2022-06-20 20:46:21 +0300

    add new fields

--- job/models.py
+++ job/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+JOB_TYPE = (
+    ('full time','full time'),
+    ('part time','part time'),
+
+)
+class job(models.Model):
+    title = models.CharField(max_length=100) 
+    job_type = models.CharField(max_length=15 , choices=JOB_TYPE)
+
